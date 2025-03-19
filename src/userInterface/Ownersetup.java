7f0ec81d17cc8b0be64408957ab2e4f069c56161
Krijen1262 <krijen.shahi101@gmail.com>
2025-03-19 20:19:33 +0545

    Added appointment,owneset,Vetassign
    Added appointment,owneset,Vetassign

---
+++ src/userInterface/Ownersetup.java
@@ -0,0 +1,134 @@
+package userInterface;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Ownersetup extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Label lblTitle, lblOwnerinfo, lblOwnername, lblGender, lblAddress, lblPetinfo, lblNickname, lblPetbreed,
+				lblDateofbirth;
+		TextField txtOwnername, txtGender, txtAddress, txtNickname, txtPetbreed, txtDateofbirth;
+		Button btnSubmit, btnCancel;
+
+		Font font = new Font("Arial", 25);
+		Font font1 = new Font("Arial", 30);
+		Font font2 = new Font("Arial", 28);
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane);
+
+		primaryStage.setScene(scene);
+		primaryStage.setWidth(1440);
+		primaryStage.setHeight(800);
+
+		primaryStage.show();
+
+		// Sidebar Background
+		Rectangle sidebar = new Rectangle(0, 0, 250, 800);
+		sidebar.setFill(Color.BLACK);
+
+		// Sidebar Labels
+		Label lblSidebarTitle = new Label("ABC CLINIC");
+		lblSidebarTitle.setFont(new Font("Arial", 24));
+		lblSidebarTitle.setTextFill(Color.WHITE);
+		lblSidebarTitle.relocate(50, 50);
+
+		lblTitle = new Label("Owner Setup");
+		lblTitle.relocate(347, 44);
+		lblTitle.setFont(font1);
+
+		lblOwnerinfo = new Label("Owner Information");
+		lblOwnerinfo.relocate(440, 160);
+		lblOwnerinfo.setFont(font2);
+
+		lblOwnername = new Label("Owner Name");
+		lblOwnername.relocate(456, 231);
+		lblOwnername.setFont(font);
+
+		txtOwnername = new TextField();
+		txtOwnername.relocate(650, 215);
+		txtOwnername.setPrefSize(311, 40);
+
+		lblGender = new Label("Gender");
+		lblGender.relocate(456, 295);
+		lblGender.setFont(font);
+
+		txtGender = new TextField();
+		txtGender.relocate(650, 281);
+		txtGender.setPrefSize(311, 40);
+
+		lblAddress = new Label("Address");
+		lblAddress.relocate(451, 359);
+		lblAddress.setFont(font);
+
+		txtAddress = new TextField();
+		txtAddress.relocate(650, 349);
+		txtAddress.setPrefSize(311, 40);
+
+		lblPetinfo = new Label("Pet Information");
+		lblPetinfo.relocate(440, 434);
+		lblPetinfo.setFont(font);
+
+		lblNickname = new Label("Pet NickName");
+		lblNickname.relocate(456, 500);
+		lblNickname.setFont(font);
+
+		txtNickname = new TextField();
+		txtNickname.relocate(650, 484);
+		txtNickname.setPrefSize(311, 40);
+
+		lblPetbreed = new Label("Pet Breed");
+		lblPetbreed.relocate(456, 566);
+		lblPetbreed.setFont(font);
+
+		txtPetbreed = new TextField();
+		txtPetbreed.relocate(650, 550);
+		txtPetbreed.setPrefSize(311, 40);
+
+		lblDateofbirth = new Label("Date of Birth");
+		lblDateofbirth.relocate(456, 632);
+		lblDateofbirth.setFont(font);
+
+		txtDateofbirth = new TextField();
+		txtDateofbirth.relocate(650, 616);
+		txtDateofbirth.setPrefSize(311, 40);
+
+		btnSubmit = new Button("Submit");
+		btnSubmit.relocate(645, 700);
+//		btnSubmit.setPrefSize(311, 40);
+
+		btnCancel = new Button("Cancel");
+		btnCancel.relocate(768, 700);
+//		btnCancel.setPrefSize(311, 40);
+
+		pane.getChildren().add(lblTitle);
+		pane.getChildren().add(lblOwnerinfo);
+		pane.getChildren().addAll(lblOwnername, txtOwnername);
+		pane.getChildren().addAll(lblGender, txtGender);
+		pane.getChildren().addAll(lblAddress, txtAddress);
+		pane.getChildren().addAll(lblPetinfo);
+		pane.getChildren().addAll(lblNickname, txtNickname);
+		pane.getChildren().addAll(lblPetbreed, txtPetbreed);
+		pane.getChildren().addAll(lblDateofbirth, txtDateofbirth);
+		pane.getChildren().add(btnSubmit);
+		pane.getChildren().add(btnCancel);
+		pane.getChildren().add(sidebar);
+		pane.getChildren().add(lblSidebarTitle);
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
---
+++ src/userInterface/VetAssign.java
@@ -0,0 +1,104 @@
+package userInterface;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class VetAssign extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Label lblTitle, lblOwnername, lblPetname, lblVetstaffname, lblStafftype;
+		TextField txtOwnername, txtPetname, txtVetstaffname, txtStafftype;
+		Button btnSubmit, btnCancel;
+
+		Font font = new Font("Arial", 25);
+		Font font1 = new Font("Arial", 30);
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane);
+
+		primaryStage.setScene(scene);
+		primaryStage.setWidth(1440);
+		primaryStage.setHeight(800);
+
+		primaryStage.show();
+
+		// Sidebar Background
+		Rectangle sidebar = new Rectangle(0, 0, 250, 800);
+		sidebar.setFill(Color.BLACK);
+
+		// Sidebar Labels
+		Label lblSidebarTitle = new Label("ABC CLINIC");
+		lblSidebarTitle.setFont(new Font("Arial", 24));
+		lblSidebarTitle.setTextFill(Color.WHITE);
+		lblSidebarTitle.relocate(50, 50);
+
+		lblTitle = new Label("Vet Assignment");
+		lblTitle.relocate(347, 44);
+		lblTitle.setFont(font1);
+
+		lblOwnername = new Label("Owner Name");
+		lblOwnername.relocate(451, 169);
+		lblOwnername.setFont(font);
+
+		txtOwnername = new TextField();
+		txtOwnername.relocate(645, 169);
+		txtOwnername.setPrefSize(311, 40);
+
+		lblPetname = new Label("Gender");
+		lblPetname.relocate(451, 238);
+		lblPetname.setFont(font);
+
+		txtPetname = new TextField();
+		txtPetname.relocate(645, 238);
+		txtPetname.setPrefSize(311, 40);
+
+		lblVetstaffname = new Label("Contact No");
+		lblVetstaffname.relocate(451, 309);
+		lblVetstaffname.setFont(font);
+
+		txtVetstaffname = new TextField();
+		txtVetstaffname.relocate(645, 309);
+		txtVetstaffname.setPrefSize(311, 40);
+
+		lblStafftype = new Label("Staff type");
+		lblStafftype.relocate(451, 376);
+		lblStafftype.setFont(font);
+
+		txtStafftype = new TextField();
+		txtStafftype.relocate(645, 376);
+		txtStafftype.setPrefSize(311, 40);
+
+		btnSubmit = new Button("Submit");
+		btnSubmit.relocate(645, 700);
+//		btnSubmit.setPrefSize(311, 40);
+
+		btnCancel = new Button("Cancel");
+		btnCancel.relocate(768, 700);
+//		btnCancel.setPrefSize(311, 40);
+
+		pane.getChildren().add(lblTitle);
+		pane.getChildren().addAll(lblOwnername, txtOwnername);
+		pane.getChildren().addAll(lblPetname, txtPetname);
+		pane.getChildren().addAll(lblVetstaffname, txtVetstaffname);
+		pane.getChildren().addAll(lblStafftype, txtStafftype);
+		pane.getChildren().add(btnSubmit);
+		pane.getChildren().add(btnCancel);
+		pane.getChildren().add(sidebar);
+		pane.getChildren().add(lblSidebarTitle);
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
